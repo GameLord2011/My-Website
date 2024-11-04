021ae06f8fefb8935eda67ea5b97e2aff66f9dde
GameLord2011 <dangerb2011@gmail.com>
2024-11-04 13:51:54 -0500

    change to tsparticles.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 
-particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+tsparticlesJS.load('particles-js', 'assets/js/particles.json', function() {
   console.log('particles.js loaded - callback');
 });
 
