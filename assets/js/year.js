6f06419751b23d130fdc713c21212b1633b8718e
GameLord2011 <dangerb2011@gmail.com>
2024-11-04 14:25:31 -0500

    tests

--- assets/js/year.js
+++ assets/js/year.js
@@ -1,3 +1,3 @@
 var currentYear = new Date().getFullYear();
 var age = (currentYear - 2011);
-document.getElementById("age_bld").innerHTML = age + "";
+document.getElementById("age_bld").innerHTML = 12;
