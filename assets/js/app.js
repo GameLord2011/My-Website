119c0a85e5cb3091d989618ca15fd99309ed52c4
GameLord2011 <dangerb2011@gmail.com>
2024-11-08 14:09:01 -0500

    joe s

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,10 @@
-// @path-json can be an object or an array, the first will be loaded directly, and the object from the array will be randomly selected
-/* tsParticles.load(@params); */
-
-tsParticles
-    .load({
-        id: "tsparticles",
-        url: "./assets/json/particles.json",
-    })
-    .then(container => {
-        console.log("callback - tsparticles config loaded");
-    })
-    .catch(error => {
-        console.error(error);
+(async () => {
+    await loadFull(tsParticles);
+  
+    await tsParticles.load({
+      id: "tsparticles",
+      options: {
+        /* options */
+      },
     });
+  })();
