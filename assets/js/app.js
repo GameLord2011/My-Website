6c5c43089510fcb7f71a3fe3186ee89506bc1d4a
GameLord2011 <dangerb2011@gmail.com>
2024-11-08 13:56:13 -0500

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 // @path-json can be an object or an array, the first will be loaded directly, and the object from the array will be randomly selected
 /* tsParticles.load(@params); */
+import { tsParticles } from "@tsparticles/engine";
 
 tsParticles
     .load({
