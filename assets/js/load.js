de504bc35a580df238cfa1ee9d5cc86b1c4b6c39
GameLord2011 <dangerb2011@gmail.com>
2024-11-14 14:42:51 -0500

    change(until I cna fix)

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,10 +1,5 @@
-import { tsParticles } from "@tsparticles/engine";
-
-/* await */ tsParticles.load({
-     element: "tsparticles", /* an HTML element used as container for the canvas, if a canvas is passed, is used that instead of a generated canvas, optional */
-    // id: /* a string to identify the current loading instance, if not passed, a random id is generated */
-    // index: /* a number used to choose from an item if the options are an array, optional, ignored if options is object */
-    // options: /* the options of the instance, can be an array of options object, optional if url is passed, if none is passed it's meaningless to load anything */
-     url: "assets/json/particles.json" /* the url used to load the options, uses AJAX to get the options object, the result is then used like the options object above. */
-  });
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/json/particles.json', function() {
+   console.log('callback - particles.js config loaded');
+ });
  
