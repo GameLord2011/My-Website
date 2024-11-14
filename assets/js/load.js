5c3fdbe76e56da485366305a254b4263571523f6
GameLord2011 <dangerb2011@gmail.com>
2024-11-14 14:59:43 -0500

    dsfaadfsaaa

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-tsparticlesJS.load('particles-js', 'assets/json/particles.json', function() {
+tsParticlesJS.load('particles-js', 'assets/json/particles.json', function() {
    console.log('callback - particles.js config loaded');
  });
  
