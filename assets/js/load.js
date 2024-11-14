ceb62105895480ce990b29c65d2141a0a141eb1b
GameLord2011 <dangerb2011@gmail.com>
2024-11-14 13:13:39 -0500

    fdsa

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,5 +1,5 @@
 /* await */ tsParticles.load({
-     element: "tsParticles", /* an HTML element used as container for the canvas, if a canvas is passed, is used that instead of a generated canvas, optional */
+     element: "tsparticles", /* an HTML element used as container for the canvas, if a canvas is passed, is used that instead of a generated canvas, optional */
     // id: /* a string to identify the current loading instance, if not passed, a random id is generated */
     // index: /* a number used to choose from an item if the options are an array, optional, ignored if options is object */
     // options: /* the options of the instance, can be an array of options object, optional if url is passed, if none is passed it's meaningless to load anything */
