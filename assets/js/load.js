29a5d7c4b85774140a6ca85592311140cf1edda3
GameLord2011 <dangerb2011@gmail.com>
2024-11-14 13:12:27 -0500

    fdsa

--- assets/js/load.js
+++ assets/js/load.js
@@ -3,6 +3,6 @@
     // id: /* a string to identify the current loading instance, if not passed, a random id is generated */
     // index: /* a number used to choose from an item if the options are an array, optional, ignored if options is object */
     // options: /* the options of the instance, can be an array of options object, optional if url is passed, if none is passed it's meaningless to load anything */
-     url: "./assets/json/particles.json" /* the url used to load the options, uses AJAX to get the options object, the result is then used like the options object above. */
+     url: "assets/json/particles.json" /* the url used to load the options, uses AJAX to get the options object, the result is then used like the options object above. */
   });
  
