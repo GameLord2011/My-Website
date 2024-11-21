8588509ecf7355b85f2f0a5d0e1b2d3fee49fe1f
GameLord2011 <dangerb2011@gmail.com>
2024-11-21 12:35:49 -0500

    fdsafa

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,9 +1,13 @@
 import { tsParticles } from "tsparticles-engine";
 
-tsParticles.load({
-  id: "tsparticles",
-  //options: {
-  //    /* options here */
-  //},
-   url: "assets\\json\\particles.json"
-});
+tsParticles
+  .load({
+    id: "tsparticles",
+    url: "assets/json/particles.json",
+  })
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
