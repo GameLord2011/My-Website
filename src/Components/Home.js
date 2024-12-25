f7e55330a5f462b7f33f09690536ddfee9c4cf6e
GameLord2011 <dangerb2011@gmail.com>
2024-12-24 19:23:21 -0500

    Changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -30,21 +30,60 @@ function Age({ Impress_Da_Girls_Mode, DOB , Age_You_Started_Programming_This_Web
     console.log("Contarary to what (I Think) you (might have) thought, I am in fact " + age + " years old.");
   }
   return(
-    <b>
+    <>
       {age}
-    </b>
+    </>
   )
 }
 
 function Home() {
   return (
     <>
-      <div className="center_panel">
-            <h1>
-                I'm <b className="name">@GameLord2011</b>
+      <div style={{
+        position: "absolute",
+        width: '70%',
+        height: '20%',
+        background: 'rgba(0, 0, 0, 0)',
+        top: '25%',
+        left: '21%',
+        margin: '-110px 0 0 -100px',
+        padding: '20px',
+        borderRadius: '4px',
+        borderStyle: 'dotted',
+        borderColor: 'darkgreen',
+        boxSizing: 'border-box',
+        textAlign: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        alignContent: 'center'
+      }}>
+            <h1 style={{
+              textAlign: 'center'
+            }}>
+                I'm <b style={{
+                  background: 'radial-gradient(circle, rgba(255,0,0,1) 0%, rgba(255,165,0,1) 16%, rgba(255,255,0,1) 32%, rgba(0,255,0,1) 48%, rgba(0,0,255,1) 64%, rgba(75,0,130,1) 80%, rgba(128,0,128,1) 100%)',
+                  color: '#000000',
+                  borderStyle: 'double',
+                  zIndex: 0
+                }}>@GameLord2011</b>
             </h1>
       </div>
-      <div className="center_panel_mn">
+      <div style={{
+        position: 'absolute',
+        width: '25%',
+        height: '20%',
+        background: 'rgba(0, 0, 0, 0)',
+        top: '45%',
+        left: '43%',
+        margin: '-110px 0 0 -100px',
+        padding: '20px',
+        borderRadius: '4px',
+        boxSizing: 'border-box',
+        textAlign: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        alignContent: 'center'
+      }}>
             <p>I am a <Age Impress_Da_Girls_Mode={false} DOB='2011-09-23' Age_You_Started_Programming_This_Website={13} /> year old ameture programmer.</p>
             <p>I program in:</p>
             <ul>
