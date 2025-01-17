4ddfbc77733d1493b6cbe486bd59b6091fb0b338
GameLord2011 <dangerb2011@gmail.com>
2025-01-17 12:00:17 -0500

    ✨ Update NotFound component to remove unnecessary classes and improve readability

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className='text-center dark:text-white'>
+    <div>
       <h2 className='font-extrabold'>&#34;Not Here!&#34;</h2>
       <p className='font-extralight'> -Dr. Dolittle</p>
       <p>I dunno where you got this link, but it&#39;s wrong, or broken from my &#40;constant&#41; site updates.</p>
