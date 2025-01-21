d1f5812d781c191c482716115611a87bdca229e2
GameLord2011 <dangerb2011@gmail.com>
2025-01-21 14:52:37 -0500

    ✨ Simplify Link component in Page by removing unnecessary class properties

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
             <p className="text-center">
                 You wern&#39;t supposed to find this page!
                 <br/>
-                <Link href="/" className="text-emerald-800 dark:text-emerald-400">
+                <Link href="/">
                     Go Home?
                 </Link>
             </p>
