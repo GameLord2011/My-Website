e7c4a7bd4e60389f58d53a601a6bb31f57773d4b
GameLord2011 <dangerb2011@gmail.com>
2025-01-22 13:10:20 -0500

    ✨ Remove unnecessary whitespace in HPageIs component for cleaner code

--- app/api/HPageIs.tsx
+++ app/api/HPageIs.tsx
@@ -30,7 +30,6 @@ export default function HPageIs() {
           <Link href='https://github-readme-stats.vercel.app/'>
             <div className="bg-svgimg w-[467px] h-[195px] bottom-0 p-0 m-0 right-0 fixed" />
           </Link>
-          
           {process.env.NODE_ENV === 'production' && bot === false && (
             <Link href="http://s01.flagcounter.com/more/ugnH" className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block w-auto h-auto">
               <Image
