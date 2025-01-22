00036967c2681afda12e94ea631b10c10777599e
GameLord2011 <dangerb2011@gmail.com>
2025-01-22 12:57:50 -0500

    ✨ Add isbot detection to HPageIs component and update dependencies

--- app/api/HPageIs.tsx
+++ app/api/HPageIs.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import Image from "next/image";
 import Link from 'next/link';
 import { isBrowser, isMobile } from "react-device-detect";
+import { isbot } from 'isbot';
 
 export default function HPageIs() {
   const [isClient, setIsClient] = useState(false);
@@ -16,6 +17,8 @@ export default function HPageIs() {
     return null;
   }
 
+  const bot = isbot(navigator.userAgent);
+
   return (
     <>
       {isBrowser && (
@@ -23,7 +26,8 @@ export default function HPageIs() {
           <Link href='https://github-readme-stats.vercel.app/'>
             <div className="bg-svgimg w-[467px] h-[195px] bottom-0 p-0 m-0 right-0 fixed" />
           </Link>
-          {process.env.NODE_ENV === 'production' && (
+          
+          {process.env.NODE_ENV === 'production' && bot === false && (
             <Link href="http://s01.flagcounter.com/more/ugnH" className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block w-auto h-auto">
               <Image
                 className="Flag_cntr"
@@ -34,7 +38,7 @@ export default function HPageIs() {
               />
             </Link>
           )}
-          {process.env.NODE_ENV === 'development' && (
+          {process.env.NODE_ENV === 'development' && bot === false && (
               <Link href="http://s05.flagcounter.com/more/xU6" className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block w-auto h-auto">
                 <Image
                   className="Flag_cntr"
@@ -49,7 +53,7 @@ export default function HPageIs() {
       )}
       {isMobile && (
         <div>
-          {process.env.NODE_ENV === 'production' && (
+          {process.env.NODE_ENV === 'production' && bot === false && (
             <Link href="http://s01.flagcounter.com/more/ugnH" className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block">
               <Image
                 className="Flag_cntr"
@@ -60,7 +64,7 @@ export default function HPageIs() {
               />
           </Link>
           )}
-          {process.env.NODE_ENV === 'development' && (
+          {process.env.NODE_ENV === 'development' && bot === false && (
               <Link href="http://s05.flagcounter.com/more/xU6" className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block">
                 <Image
                   className="Flag_cntr"
