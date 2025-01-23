30ea0dae97144ea878bbae9ca1f0fd89179f5fd8
GameLord2011 <dangerb2011@gmail.com>
2025-01-23 15:53:40 -0500

    ✨ Change <i> tags to <span> for text formatting consistency in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   React.useEffect(() => {
     const typed = new Typed(el.current, {
       strings: [
-        '<i class="underline decoration-red-700 decoration-wavy decoration-1 underline-offset-1">amature</i> programmer', 
-        '<i class="underline decoration-red-700 decoration-wavy decoration-1 underline-offset-1">amature</i> frontend / backend web dev', 
-        '<i class="underline decoration-red-700 decoration-wavy decoration-1 underline-offset-1">amature</i> C / C++ / C# developer', 
+        '<span class="underline decoration-red-700 decoration-wavy decoration-1 underline-offset-1">amature</span> programmer', 
+        '<span class="underline decoration-red-700 decoration-wavy decoration-1 underline-offset-1">amature</span> frontend / backend web dev', 
+        '<span class="underline decoration-red-700 decoration-wavy decoration-1 underline-offset-1">amature</span> C / C++ / C# developer', 
         'developer'
       ],
       typeSpeed: 90,
