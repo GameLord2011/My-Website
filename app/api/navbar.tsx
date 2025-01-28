b4324520c0c37dca34d566836546e473148f98c8
GameLord2011 <dangerb2011@gmail.com>
2025-01-28 14:20:55 -0500

    ✨ Enhance navbar styles with hover animations and new color variables

--- app/api/navbar.tsx
+++ app/api/navbar.tsx
@@ -17,9 +17,7 @@ export default function Navbar() {
         <div className="dropdown-content">
           {pathname === "/" ? null : <Link href="/">Home</Link>}
           {pathname === "/about" ? null : <Link href="/about">About</Link>}
-          {pathname === "/shoutouts" ? null : (
-            <Link href="/shoutouts">Shoutouts</Link>
-          )}
+          {pathname === "/shoutouts" ? null : <Link href="/shoutouts">Shoutouts</Link>}
           {pathname === "/repos" ? null : <Link href="/repos">Repos</Link>}
           <Link href="https://linktr.ee/GameLord2011">Links</Link>
         </div>
