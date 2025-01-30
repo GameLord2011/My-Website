d01aaf44ed72a7d943fae1a7db226c5bfb7dedfc
GameLord2011 <dangerb2011@gmail.com>
2025-01-30 12:34:54 -0500

    Remove flag counters from site cuz ~40% of traffic is web crawlers.

--- app/api/HPageIs.tsx
+++ app/api/HPageIs.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
-import { isBrowser, isMobile } from "react-device-detect";
+import { isBrowser } from "react-device-detect";
 import { isbot } from "isbot";
 
 export default function HPageIs() {
@@ -30,70 +29,6 @@ export default function HPageIs() {
           <Link href="https://github-readme-stats.vercel.app/">
             <div className="bg-svgimg w-[467px] h-[195px] bottom-0 p-0 m-0 right-0 fixed" />
           </Link>
-          {process.env.NODE_ENV === "production" && bot === false && (
-            <Link
-              href="http://s01.flagcounter.com/more/ugnH"
-              className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block w-auto h-auto"
-            >
-              <Image
-                className="Flag_cntr w-auto h-auto bz30:block hidden"
-                src="https://s01.flagcounter.com/count2/ugnH/bg_000000/txt_1e4d12/border_1E4D12/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/"
-                alt="Flag Counter"
-                width={0}
-                height={0}
-                loading="lazy"
-              />
-            </Link>
-          )}
-          {process.env.NODE_ENV === "development" && bot === false && (
-            <Link
-              href="http://s05.flagcounter.com/more/xU6"
-              className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block w-auto h-auto"
-            >
-              <Image
-                className="Flag_cntr w-auto h-auto bz30:block hidden"
-                src="https://s05.flagcounter.com/count2_US/xU6/bg_000000/txt_044002/border_044002/columns_2/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"
-                alt="Flag Counter"
-                width={0}
-                height={0}
-                loading="lazy"
-              />
-            </Link>
-          )}
-        </div>
-      )}
-      {isMobile && (
-        <div>
-          {process.env.NODE_ENV === "production" && bot === false && (
-            <Link
-              href="http://s01.flagcounter.com/more/ugnH"
-              className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block"
-            >
-              <Image
-                className="Flag_cntr w-auto h-auto bz30:block hidden"
-                src="https://s01.flagcounter.com/count2/ugnH/bg_000000/txt_1e4d12/border_1E4D12/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/"
-                alt="Flag Counter"
-                width={0}
-                height={0}
-                loading="lazy"
-              />
-            </Link>
-          )}
-          {process.env.NODE_ENV === "development" && bot === false && (
-            <Link
-              href="http://s05.flagcounter.com/more/xU6"
-              className="hidden bottom-0 p-0 m-0 left-0 fixed dark:block"
-            >
-              <Image
-                className="Flag_cntr w-auto h-auto bz30:block hidden"
-                src="https://s05.flagcounter.com/count2_US/xU6/bg_000000/txt_044002/border_044002/columns_2/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"
-                alt="Flag Counter"
-                width={0}
-                height={0}
-                loading="lazy"
-              />
-            </Link>
-          )}
         </div>
       )}
     </>
--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s05.flagcounter.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "s01.flagcounter.com",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
