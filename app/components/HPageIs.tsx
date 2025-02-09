ef7016b727ad4299213248e0ce1e322db39b4fa9
GameLord2011 <dangerb2011@gmail.com>
2025-02-08 19:26:29 -0500

    Small changes (update deps, and account for thinner pc screens)

--- app/components/HPageIs.tsx
+++ app/components/HPageIs.tsx
@@ -27,7 +27,7 @@ export default function HPageIs() {
       {isBrowser && !bot && (
         <div>
           <Link href="https://github-readme-stats.vercel.app/">
-            <div className="bg-svgimg fixed right-0 bottom-0 m-0 h-[195px] w-[467px] p-0" />
+            <div className="bg-svgimg hidden fixed right-0 bottom-0 m-0 h-[195px] w-[467px] p-0 minsvgw:block" />
           </Link>
         </div>
       )}
