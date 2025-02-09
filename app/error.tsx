2bcb8417b0d257c1010990e4be6bcdd8c9b7eb09
GameLord2011 <dangerb2011@gmail.com>
2025-02-08 19:36:21 -0500

    Refactor error handling by replacing GlobalError component and implementing ErrorBoundary

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+import GlobalError from '@/app/global-error';
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return <GlobalError error={error} reset={reset} />;
+}
