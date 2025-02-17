0aad8d50f502f3593b00ed0a5cc31ee02c89a485
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-02-17 12:55:44 -0500

    Undo commit #218d01f
    Undoes changes from commit #218d01f

--- app/repos/page.tsx
+++ app/repos/page.tsx
@@ -88,9 +88,9 @@ export default function Page() {
             src={emojis[part.slice(1, -1)]}
             alt={part}
             title={part}
-            width={20}
-            height={20}
-            className="inline-flex"
+            width={0}
+            height={0}
+            className="inline-flex h-[1em] w-[1em]"
             loading="eager"
           />
         );
