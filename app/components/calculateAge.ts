2d91f6936e72a0f2be5cc70f4853999aec2d7fed
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-02-20 13:33:07 -0500

    Undo commits fd418e - 81ebadf

--- app/components/calculateAge.ts
+++ app/components/calculateAge.ts
@@ -1,39 +1,19 @@
 "use client";
 
 export const calculateAge = async (birthdate: string): Promise<number> => {
-  try {
-    const ipResponse = await fetch("https://api.ipify.org?format=json");
-    if (!ipResponse.ok) {
-      throw new Error("Failed to fetch IP address");
-    }
-
-    const ipData = await ipResponse.json();
-    const ipAddress = ipData.ip;
-    const response = await fetch(
-      `https://www.timeapi.io/api/time/current/ip?ipAddress=${ipAddress}`,
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch current date and time");
-    }
-
-    const data = await response.json();
-    const currentDate = new Date(data.dateTime);
-    console.info(`Date/Time: ${currentDate}`);
-
+  return new Promise((resolve) => {
     const birthDate = new Date(birthdate);
-    let age = currentDate.getFullYear() - birthDate.getFullYear();
-    const monthDifference = currentDate.getMonth() - birthDate.getMonth();
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
 
     if (
       monthDifference < 0 ||
-      (monthDifference === 0 && currentDate.getDate() < birthDate.getDate())
+      (monthDifference === 0 && today.getDate() < birthDate.getDate())
     ) {
       age--;
     }
 
-    return age;
-  } catch (error) {
-    console.error("Error calculating age:", error);
-    throw error;
-  }
+    resolve(age);
+  });
 };
