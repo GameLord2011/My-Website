31dc02684b9ceaac4e64e434bbf2722cb5f53425
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-02-21 11:55:12 -0500

    Prettier run.

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -23,36 +23,98 @@ export default function Navbar() {
   return (
     <>
       {isBrowser && (
-        <nav className="relative z-[10000] @container">
-          <div className="dropdown inline-block sticky right-full bottom-full group">
+        <nav className="@container relative z-[10000]">
+          <div className="dropdown group sticky right-full bottom-full inline-block">
             <button
               type="button"
-              className="dropbtn bg-[var(--nav-btn-bg)] text-[hsl(0,0%,93%)] p-4 text-lg border-none cursor-pointer top-full left-full z-[1] rounded-r-md hover:animate-btn-hvr"
+              className="dropbtn hover:animate-btn-hvr top-full left-full z-[1] cursor-pointer rounded-r-md border-none bg-[var(--nav-btn-bg)] p-4 text-lg text-[hsl(0,0%,93%)]"
             >
               Pages
             </button>
-            <div className="dropdown-content hidden absolute bg-[var(--nav-bkg)] min-w-[150px] shadow-lg z-[1] rounded-md rounded-tl-none group-hover:block">
-              {pathname === "/" ? null : <Link href="/" className="block px-[12px] py-[16px] hover:animate-nvbr-lnk-hvr">Home</Link>}
-              {pathname === "/about" ? null : <Link href="/about" className="block px-[12px] py-[16px] hover:animate-nvbr-lnk-hvr">About</Link>}
-              {pathname === "/shoutouts" ? null : (
-                <Link href="/shoutouts" className="block px-[12px] py-[16px] hover:animate-nvbr-lnk-hvr">Shoutouts</Link>
+            <div className="dropdown-content absolute z-[1] hidden min-w-[150px] rounded-md rounded-tl-none bg-[var(--nav-bkg)] shadow-lg group-hover:block">
+              {pathname === "/" ? null : (
+                <Link
+                  href="/"
+                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px]"
+                >
+                  Home
+                </Link>
               )}
-              {pathname === "/repos" ? null : <Link href="/repos" className="block px-[12px] py-[16px] hover:animate-nvbr-lnk-hvr">Repos</Link>}
-              <Link href="https://linktr.ee/GameLord2011" className="block px-[12px] py-[16px] hover:animate-nvbr-lnk-hvr">Links</Link>
+              {pathname === "/about" ? null : (
+                <Link
+                  href="/about"
+                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px]"
+                >
+                  About
+                </Link>
+              )}
+              {pathname === "/shoutouts" ? null : (
+                <Link
+                  href="/shoutouts"
+                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px]"
+                >
+                  Shoutouts
+                </Link>
+              )}
+              {pathname === "/repos" ? null : (
+                <Link
+                  href="/repos"
+                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px]"
+                >
+                  Repos
+                </Link>
+              )}
+              <Link
+                href="https://linktr.ee/GameLord2011"
+                className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px]"
+              >
+                Links
+              </Link>
             </div>
           </div>
         </nav>
       )}
       {isMobile && (
         <nav>
-          <div className="mobile-navbar-content bg-[var(--nav-bkg)] w-full top-full left-0 z-[1] sticky flex text-center justify-center">
-            {pathname === "/" ? null : <Link href="/" className="block px-[5px] py-[12px] hover:animate-nvbr-lnk-hvr @max-[240px]:px-[12px] @max[240px]:py-[16px]">Home</Link>}
-            {pathname === "/about" ? null : <Link href="/about" className="block px-[5px] py-[12px] hover:animate-nvbr-lnk-hvr @max-[240px]:px-[12px] @max[240px]:py-[16px]">About</Link>}
-            {pathname === "/shoutouts" ? null : (
-              <Link href="/shoutouts" className="block px-[5px] py-[12px] hover:animate-nvbr-lnk-hvr @max-[240px]:px-[12px] @max[240px]:py-[16px]">Shoutouts</Link>
+          <div className="mobile-navbar-content sticky top-full left-0 z-[1] flex w-full justify-center bg-[var(--nav-bkg)] text-center">
+            {pathname === "/" ? null : (
+              <Link
+                href="/"
+                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] @max-[240px]:px-[12px]"
+              >
+                Home
+              </Link>
             )}
-            {pathname === "/repos" ? null : <Link href="/repos" className="block px-[5px] py-[12px] hover:animate-nvbr-lnk-hvr @max-[240px]:px-[12px] @max[240px]:py-[16px]">Repos</Link>}
-            <Link href="https://linktr.ee/GameLord2011" className="block px-[5px] py-[12px] @max-[240px]:px-[12px] @max[240px]:py-[16px]">Links</Link>
+            {pathname === "/about" ? null : (
+              <Link
+                href="/about"
+                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] @max-[240px]:px-[12px]"
+              >
+                About
+              </Link>
+            )}
+            {pathname === "/shoutouts" ? null : (
+              <Link
+                href="/shoutouts"
+                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] @max-[240px]:px-[12px]"
+              >
+                Shoutouts
+              </Link>
+            )}
+            {pathname === "/repos" ? null : (
+              <Link
+                href="/repos"
+                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] @max-[240px]:px-[12px]"
+              >
+                Repos
+              </Link>
+            )}
+            <Link
+              href="https://linktr.ee/GameLord2011"
+              className="@max[240px]:py-[16px] block px-[5px] py-[12px] @max-[240px]:px-[12px]"
+            >
+              Links
+            </Link>
           </div>
         </nav>
       )}
