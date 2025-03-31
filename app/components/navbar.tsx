fd5c813ae83b7238cc85eebe4c0091352221708a
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-03-31 13:38:58 -0400

    Add clsx to dynammicly hide links better.

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,6 +5,30 @@ import { isBrowser, isMobile } from "react-device-detect";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import ThemeSwitcher from "./themeswitcher";
+import clsx from "clsx";
+
+const links = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Shoutouts",
+    href: "/shoutouts",
+  },
+  {
+    name: "Repos",
+    href: "/repos",
+  },
+  {
+    name: "Links",
+    href: "https://linktr.ee/GameLord2011",
+  },
+];
 
 export default function Navbar() {
   const pathname: string = usePathname();
@@ -33,44 +57,20 @@ export default function Navbar() {
               Pages
             </button>
             <div className="dropdown-content absolute hidden min-w-[150px] rounded-md rounded-tl-none bg-[var(--nav-bkg)] shadow-lg transition-all duration-500 ease-in-out group-hover:block">
-              {pathname === "/" ? null : (
+              {links.map((link) => (
                 <Link
-                  href="/"
-                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px] transition-all duration-500 ease-in-out"
+                  href={link.href}
+                  className={clsx(
+                    "hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px] transition-all duration-500 ease-in-out",
+                    {
+                      hidden: pathname === link.href,
+                    },
+                  )}
+                  key={link.name}
                 >
-                  Home
+                  {link.name}
                 </Link>
-              )}
-              {pathname === "/about" ? null : (
-                <Link
-                  href="/about"
-                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px] transition-all duration-500 ease-in-out"
-                >
-                  About
-                </Link>
-              )}
-              {pathname === "/shoutouts" ? null : (
-                <Link
-                  href="/shoutouts"
-                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px] transition-all duration-500 ease-in-out"
-                >
-                  Shoutouts
-                </Link>
-              )}
-              {pathname === "/repos" ? null : (
-                <Link
-                  href="/repos"
-                  className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px] transition-all duration-500 ease-in-out"
-                >
-                  Repos
-                </Link>
-              )}
-              <Link
-                href="https://linktr.ee/GameLord2011"
-                className="hover:animate-nvbr-lnk-hvr block px-[12px] py-[16px] transition-all duration-500 ease-in-out"
-              >
-                Links
-              </Link>
+              ))}
             </div>
           </div>
           <div className="flex items-center">
@@ -81,44 +81,20 @@ export default function Navbar() {
       {isMobile && (
         <nav>
           <div className="mobile-navbar-content sticky top-full left-0 z-[1] flex w-full justify-center bg-[var(--nav-bkg)] text-center">
-            {pathname === "/" ? null : (
+            {links.map((link) => (
               <Link
-                href="/"
-                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] transition-all duration-500 ease-in-out @max-[240px]:px-[12px]"
+                href={link.href}
+                className={clsx(
+                  "hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] transition-all duration-500 ease-in-out @max-[240px]:px-[12px]",
+                  {
+                    hidden: pathname === link.href,
+                  },
+                )}
+                key={link.name}
               >
-                Home
+                {link.name}
               </Link>
-            )}
-            {pathname === "/about" ? null : (
-              <Link
-                href="/about"
-                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] transition-all duration-500 ease-in-out @max-[240px]:px-[12px]"
-              >
-                About
-              </Link>
-            )}
-            {pathname === "/shoutouts" ? null : (
-              <Link
-                href="/shoutouts"
-                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] transition-all duration-500 ease-in-out @max-[240px]:px-[12px]"
-              >
-                Shoutouts
-              </Link>
-            )}
-            {pathname === "/repos" ? null : (
-              <Link
-                href="/repos"
-                className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] transition-all duration-500 ease-in-out @max-[240px]:px-[12px]"
-              >
-                Repos
-              </Link>
-            )}
-            <Link
-              href="https://linktr.ee/GameLord2011"
-              className="hover:animate-nvbr-lnk-hvr @max[240px]:py-[16px] block px-[5px] py-[12px] transition-all duration-500 ease-in-out @max-[240px]:px-[12px]"
-            >
-              Links
-            </Link>
+            ))}
             <ThemeSwitcher />
           </div>
         </nav>
