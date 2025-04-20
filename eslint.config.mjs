9fe25f9eefce68dd33cf8b988cf3f71fa2bcf369
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-04-20 13:35:30 -0400

    Junk

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      ".next/",
+      "coverage/",
+      "*.log",
+      "*.lock",
+    ],
+  },
 ];
 
 export default eslintConfig;
