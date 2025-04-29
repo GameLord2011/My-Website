11f7bbb041915d6390228e51867686b4959c0491
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-04-29 15:26:05 -0400

    Fix some junk.

--- app/components/TSParticles.tsx
+++ app/components/TSParticles.tsx
@@ -2,11 +2,18 @@
 
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { tsParticles } from "@tsparticles/engine";
 import type { Container } from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
+import { usePathname, useSearchParams } from "next/navigation"; // Use these hooks to detect route changes
+
+export const dynamic = "force-dynamic";
 
 export default function TSParticles() {
   const [init, setInit] = useState(false);
+  const pathname = usePathname(); // Detects the current path
+  const searchParams = useSearchParams(); // Detects query parameters
+  const particles = tsParticles.domItem(0);
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
@@ -15,6 +22,16 @@ export default function TSParticles() {
       setInit(true);
     });
   }, []);
+
+  useEffect(() => {
+    if (!particles) return;
+
+    // Pause particles when the route changes
+    particles.pause();
+
+    // Play particles after the route change is complete
+    particles.play();
+  }, [pathname, searchParams, particles]); // Trigger this effect when the path or query parameters change
 
   const particlesLoaded = async (container?: Container) => {
     console.info(container);
