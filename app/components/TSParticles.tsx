3c532e89ae785a1c7d62991f7c5ff1b333d3969e
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-04-29 15:57:21 -0400

    Remobe paritcles background from pokesearch.

--- app/components/TSParticles.tsx
+++ app/components/TSParticles.tsx
@@ -2,20 +2,17 @@
 
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { tsParticles } from "@tsparticles/engine";
 import type { Container } from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
-import { usePathname, useSearchParams } from "next/navigation"; // Use these hooks to detect route changes
+import { usePathname } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default function TSParticles() {
   const [init, setInit] = useState(false);
-  const pathname = usePathname(); // Detects the current path
-  const searchParams = useSearchParams(); // Detects query parameters
-  const particles = tsParticles.domItem(0);
+  const pathname = usePathname();
 
-  useEffect(() => { 
+  useEffect(() => {
     initParticlesEngine(async (engine) => {
       await loadSlim(engine);
     }).then(() => {
@@ -23,25 +20,13 @@ export default function TSParticles() {
     });
   }, []);
 
-  useEffect(() => {
-    if (!particles) return;
-
-    // Debounce the pause/play logic to prevent strobing
-    const timeout = setTimeout(() => {
-      particles.pause();
-      particles.play();
-    }, 100); // Adjust the delay as needed
-
-    return () => clearTimeout(timeout); // Cleanup the timeout
-  }, [pathname, searchParams, particles]);
-
   const particlesLoaded = async (container?: Container) => {
     console.info(container);
   };
 
   return (
     <>
-      {init && (
+      {init && (pathname === "/other/pokesearch") && (
         <Particles
           id="tsparticles"
           url="/particles.json"
